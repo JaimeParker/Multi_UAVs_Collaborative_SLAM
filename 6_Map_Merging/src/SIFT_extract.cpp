ef4ed759687d441e6eb7e18beb9beb25c4f4a111
hazyparker_linux <2559272883@qq.com>
2022-04-25 11:11:23 +0800

    add SIFT features

---
+++ 6_Map_Merging/src/SIFT_extract.cpp
@@ -0,0 +1,30 @@
+//
+// Created by hazyparker on 2022/4/25.
+//
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/features2d.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main() {
+    Mat srcImage = imread("../image/1.jpg");
+
+    // set features number(max)
+    int numFeatures = 400;
+    Ptr<SIFT> detector = SIFT::create(numFeatures);
+
+    vector<KeyPoint> key_points;
+    detector->detect(srcImage, key_points, Mat());
+    printf("Total key-points: %zu\n", key_points.size());
+
+    Mat keypoint_img;
+    drawKeypoints(srcImage, key_points, keypoint_img, Scalar::all(-1), DrawMatchesFlags::DEFAULT);
+    imshow("keypoint_img", keypoint_img);
+    imwrite("../image/1_SIFT.jpg", keypoint_img);
+
+    waitKey(0);
+    return 0;
+}
