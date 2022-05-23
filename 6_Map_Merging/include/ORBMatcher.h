2fbd74aa557c81be4b009f41cfd0ba4c2e0931d4
hazyparker_linux <2559272883@qq.com>
2022-05-23 11:40:37 +0800

    add description for header

--- 6_Map_Merging/include/ORBMatcher.h
+++ 6_Map_Merging/include/ORBMatcher.h
@@ -11,16 +11,36 @@ using namespace std;
 
 class ORBMatcher {
 public:
-    // get ORB features
+    /**
+     * get ORB features
+     * @param img1 image1 for detect
+     * @param img2 image2 for detect
+     */
     static void findFeatures(cv::Mat &img1, cv::Mat &img2);
 
-    // compose Pose, R, t Matrix
+    /**
+     * compose Pose, R, t Matrix
+     * @param keyPoints1 vector<cv::KeyPoint>
+     * @param keyPoints2 vector<cv::KeyPoint>
+     * @param matches vector<cv::DMatch>
+     * @param R Rotation matrix R
+     * @param t Transition matrix t
+     */
     static void getPoseEstimation(vector<cv::KeyPoint> keyPoints1,
                                   vector<cv::KeyPoint> keyPoints2,
                                   vector<cv::DMatch> matches,
                                   cv::Mat &R, cv::Mat &t);
 
-    // get landmark position
+    /**
+     * get landmark position
+     * @param K cv::Mat, camera intrinsics matrix
+     * @param keypoint_1 vector<cv::KeyPoint>
+     * @param keypoint_2 vector<cv::KeyPoint>
+     * @param matches vector<cv::DMatch>
+     * @param R Rotation matrix R
+     * @param t Transition matrix t
+     * @param points vector<cv::Point3d>, hold MapPoint Position
+     */
     static void getKeyPointPosition(const cv::Mat &K,
                                     const vector<cv::KeyPoint> &keypoint_1,
                                     const vector<cv::KeyPoint> &keypoint_2,
@@ -28,10 +48,18 @@ public:
                                     const cv::Mat &R, const cv::Mat &t,
                                     vector<cv::Point3d> &points);
 
-    // pixel to camera coordination
+    /**
+     * pixel to camera coordination
+     * @param point cv::Point2d
+     * @param K cv::Mat, camera intrinsics matrix
+     * @return cv::Point2f, in camera's coordinate system
+     */
     static cv::Point2f pixel2cam(const cv::Point2d &point, const cv::Mat &K);
 
-    // plot MapPoints
+    /**
+     * plot MapPoints
+     * @param MapPoints cv::Point3d, MapPoint position
+     */
     static void showMapPoints(const vector<cv::Point3d> &MapPoints);
 };
 
