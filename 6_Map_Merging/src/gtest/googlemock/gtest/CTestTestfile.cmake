cdaef5b31d15e718ea8b0f3135ccf4b462807b6d
hazyparker <2559272883@qq.com>
2022-05-28 23:22:58 +0800

    Delete 6_Map_Merging/src/gtest directory

--- 6_Map_Merging/src/gtest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/hazyparker/project/Multi_UAVs_Collaborative_SLAM/6_Map_Merging/src/gtest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
