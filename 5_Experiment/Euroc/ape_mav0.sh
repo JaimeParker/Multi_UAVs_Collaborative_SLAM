75c8a152cb224e66e79dbea7c099ec56a53e8141
hazyparker_linux <2559272883@qq.com>
2022-06-07 20:38:17 +0800

    add evp ape for 0 and 1

---
+++ 5_Experiment/Euroc/ape_mav0.sh
@@ -0,0 +1,1 @@
+evo_ape tum data/ground_truth/GroundTruth0.tum data/trajectory/KeyFrameTrajectory0.txt -p --plot_mode=xyz --align --correct_scale
---
+++ 5_Experiment/Euroc/ape_mav1.sh
@@ -0,0 +1,1 @@
+evo_ape tum data/ground_truth/GroundTruth1.tum data/trajectory/KeyFrameTrajectory1.txt -p --plot_mode=xyz --align --correct_scale
